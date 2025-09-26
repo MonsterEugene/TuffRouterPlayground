ebf2222adbef019f166968f5ba75a6a05ae2841d
Dalt <monstereugene07@gmail.com>
2025-09-26 10:48:26 -1000

    we got sum graphs ong

--- app/brainrotcore.js
+++ app/brainrotcore.js
@@ -1,6 +1,7 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Line, Svg } from "react-native-svg";
 
 export default function Brainrotcore() {
   return (
@@ -9,6 +10,11 @@ export default function Brainrotcore() {
       <Link href="/">sig</Link>
       <Link href="javascriptpage">ma</Link>
 
+      <Svg height={100} width={100}>
+        <Line y1={0} x1={0} x2={100} y2={100} stroke={"red"} strokeWidth={2}/>
+        <Line y1={100} y2={0} x1={0} x2={100} stroke={"black"} strokeWidth={2}/>
+        <Line y1={50} y2={50} x1={0} x2={50} stroke={"grey"} strokeWidth={2} strokeDasharray={[0,1,2,4]} strokeDashoffset={3}/>
+      </Svg>
       <StatusBar style="auto" />
     </View>
   );
--- app/index.js
+++ app/index.js
@@ -1,21 +1,133 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions} from 'react-native';
 import { Link } from 'expo-router';
 import LinkButton from '../components/LinkButton';
+/* so sad ts doesnt work :(
+import {Expression, GraphingCalculator, useHelperExpression} from "desmos-react";
+*/
+/*
+function Demo() {
+  return (
+    <GraphingCalculator
+      attributes={{className: "calculator"}}
+      fontSize={18} keypad projectorMode
+    >
+      <Expression id="slider" latex="a=3" />
+      <Point />
+    </GraphingCalculator>
+  );
+}
+
+function Point() {
+  const a = useHelperExpression({latex: "a"});
+
+  let label;
+  if (a > 0)
+    label = "positive x-axis"
+  else if (a < 0)
+    label = "negative x-axis"
+  else
+    label = "origin";
+
+  return (
+    <Expression id="point" latex="(a,0)" label={label} showLabel />
+  );
+}
+*/
+import { LineChart } from "react-native-chart-kit";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>index</Text>
-      <LinkButton page ="brainrotcore" sText="sig"/>
+      <LinkButton page="brainrotcore" sText="sig" />
       <Link href="javascriptpage">ma</Link>
-      <LinkButton page = "huff" sText="f u n"></LinkButton>
+      <LinkButton page="huff" sText="f u n"></LinkButton>
       {/* somehow the () in tabs makes it know how to get directly to huff.js*/}
+      {/*<Demo></Demo>*/}
+
+      <LineChart data={{
+        labels: ["January", "February", "March", "April", "May", "June"],
+        datasets: [
+          {
+            data: [
+              Math.random() * 100,
+              Math.random() * 100,
+              Math.random() * 100,
+              Math.random() * 100,
+              Math.random() * 100,
+              Math.random() * 100
+            ]
+          }
+        ]
+      }}
+      fromZero 
+      width={Dimensions.get("window").width -700} // from react-native
+        height={220}
+        yAxisLabel="$"
+        yAxisSuffix=""
+        yAxisInterval={1} // optional, defaults to 1
+        chartConfig={{
+          backgroundColor: "#e26a00",
+          backgroundGradientFrom: "#fb8c00",
+          backgroundGradientTo: "#ffa726",
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16
+          },
+          propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#ffa726"
+          }
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16
+        }}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
 }
+/*
+import {Expression, GraphingCalculator, useHelperExpression} from "desmos-react";
 
+function Demo() {
+  return (
+    <GraphingCalculator
+      attributes={{className: "calculator"}}
+      fontSize={18} keypad projectorMode
+    >
+      <Expression id="slider" latex="a=3" />
+      <Point />
+    </GraphingCalculator>
+  );
+}
+
+/* useHelperExpression() can only be used inside <GraphingCalculator/>,
+which is why this couldn't go in <Demo/> */
+/*
+function Point() {
+  const a = useHelperExpression({latex: "a"});
+
+  let label;
+  if (a > 0)
+    label = "positive x-axis"
+  else if (a < 0)
+    label = "negative x-axis"
+  else
+    label = "origin";
+
+  return (
+    <Expression id="point" latex="(a,0)" label={label} showLabel />
+  );
+}
+*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
